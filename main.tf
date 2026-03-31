ef59e090ca89d0d5be829e177f898a0f2cf8458d
Gaurav Hoskote <gauravgourang.hoskote@phoenix.edu>
2026-03-31 17:38:51 -0500

    init

--- main.tf
+++ main.tf
@@ -4,9 +4,9 @@
 
 module "client_profile" {
   source = "./modules/bedrock_inference_profile"
-  model_arn = "var.model_arn"
-  ai_inference_profile_name = "var.ai_inference_profile_name"
-  description = "var.description"
+  model_arn = "us.anthropic.claude-sonnet-4-20250514-v1:0"
+  ai_inference_profile_name = "client-profile"
+  description = "Inference profile for financial advisor client application"
   module_key = "jpmc_fin_advisor_client"
   application_key = "jpmc_fin_advisor"
   environment = var.environment
