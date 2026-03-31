70c5508fcb04164957cba7d5968bb04f6ce78a77
Gaurav Hoskote <gauravgourang.hoskote@phoenix.edu>
2026-03-31 18:03:46 -0500

    init

--- main.tf
+++ main.tf
@@ -1,13 +1,31 @@
-
-
-
-
-module "client_profile" {
+module "client_inf_profile" {
   source = "./modules/bedrock_inference_profile"
   model_arn = "arn:aws:bedrock:us-east-1::inference-profile/global.anthropic.claude-opus-4-5-20251101-v1:0"
   ai_inference_profile_name = "client-profile"
-  description = "Inference profile for financial advisor client application"
-  module_key = "jpmc_fin_advisor_client"
-  application_key = "jpmc_fin_advisor"
+  description = "Inference profile for financial advisory CLIENT model"
+  module_key = "jpmc_fin_advisory_client"
+  application_key = "jpmc_fin_advisory"
+  environment = var.environment
+}
+
+
+module "advisor_inf_profile" {
+  source = "./modules/bedrock_inference_profile"
+  model_arn = "arn:aws:bedrock:us-east-1::inference-profile/global.anthropic.claude-opus-4-5-20251101-v1:0"
+  ai_inference_profile_name = "advisor-profile"
+  description = "Inference profile for financial advisory ADVISOR model"
+  module_key = "jpmc_fin_advisory_advisor"
+  application_key = "jpmc_fin_advisory"
+  environment = var.environment
+}
+
+
+module "analyst_inf_profile" {
+  source = "./modules/bedrock_inference_profile"
+  model_arn = "arn:aws:bedrock:us-east-1::inference-profile/global.anthropic.claude-opus-4-5-20251101-v1:0"
+  ai_inference_profile_name = "analyst-profile"
+  description = "Inference profile for financial advisory ANALYST model"
+  module_key = "jpmc_fin_advisory_analyst"
+  application_key = "jpmc_fin_advisory"
   environment = var.environment
 }
